5fdff1d9983b9d7ed9d969f1f91903cfaf602687
Junhao <junhaolicareers@gmail.com>
2023-01-29 21:15:59 -0800

    Init Project next-todo

---
+++ src/main/java/com/junhao/nexttodo/NextTodoApplication.java
@@ -0,0 +1,13 @@
+package com.junhao.nexttodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/junhao/nexttodo/NextTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junhao.nexttodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
